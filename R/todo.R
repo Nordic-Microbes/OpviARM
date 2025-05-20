97832470de70aeea2f3949cb24a327817a62d9a0
Mark Lyng <marklyng@live.dk>
2025-05-20 08:12:19 +0200

    Initial commit Writing out the raw code before converting to package

---
+++ R/todo.R
@@ -0,0 +1,32 @@
+# 1: Update Trial info
+# ✔ - Find associated trial
+# ✔ - Create 1-row dataframe with info from md
+# ✔ - PATCH (separate call)
+# 2: Update soil info
+# ✔ - Find soil record
+# ✔ - Create 1-row dataframe with info from md
+# ✔ - PATCH (separate call)
+# 3: Add assessments (check for existing records)
+# ✔ - Find treatments
+# ✔ - Check for existing assessments
+# ✔ - Remove existing assessments from ARM export
+# ✔ - Create assessment-specific dataframes (COT, Y)
+# ✔ - 1 row per plot per assessment timing
+# ✔ - COT: 1 value variable
+# ✔ - Y: 5 value variables
+# - Possibility to update assessments?
+# 4: Add maintenance (check for existing records)
+# - Find treatments
+# - Check for existing maintenance
+# - Remove existing maintenance from ARM export
+# - Add new maintenance
+# - Possibility to update
+# Implement sanity checks and user evaluation
+# - ARM Study info
+# - ARM soil info
+# - ARM Treatment info
+# - ARM Assessment info
+# - ARM Maintenance info
+# - Associated Opvia record(s)
+# - Has anything been filled in already?
+# Trial ID, Treatment code, ARM fields and structure
